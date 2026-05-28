4c53b31d14391833c3cd8e4fb2d226998d1712b9
gosse123 <gossenahounou@gmail.com>
2026-05-28 13:55:58 +0000

    modulariter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use crate::exercise_td::cinq_top;
+//use crate::exercise_td::equation_second;
+
 mod exercise_td;
 mod utils;
-use cinq_top::cinq_stop;
-
 
 fn main(){
 
-   cinq_stop();
+   cinq_top::cinq_stop();
     
 
  }
